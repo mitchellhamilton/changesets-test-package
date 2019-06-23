5da8883578c5f702b7677f82f05f38a601822bfc
mitchellhamilton <mitchell@hamil.town>
2019-06-23 21:02:22 +1000

    Try some stuff

--- filter-action/index.js
+++ filter-action/index.js
@@ -1,17 +1,40 @@
 const { Toolkit } = require("actions-toolkit");
-const spawn = require("spawndamnit");
+const _spawn = require("spawndamnit");
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  let execGit = args => {
-    return spawn("git", args, { cwd: tools.workspace });
+  let spawn = (command, args) => {
+    return _spawn(command, args, { cwd: tools.workspace });
   };
-  let { stdout, stderr } = await execGit(["checkout", "changeset-release"]);
+
+  let { stdout, stderr } = await spawn("git", [
+    "checkout",
+    "changeset-release"
+  ]);
   if (stderr) {
     console.log("creating changeset-release branch");
-    await execGit(["checkout", "-b", "changeset-release"]);
+    await spawn("git", ["checkout", "-b", "changeset-release"]);
   }
-  let { stdout } = await execGit(["status"]);
-  console.log(stdout.toString("utf8"));
-  tools.exit.neutral("No new changesets");
+
+  let cmd = await spawnGit(["merge-base", "changeset-release", "master"]);
+  const divergedAt = cmd.stdout.trim();
+
+  await spawn("git", ["diff", "--name-only", `${divergedAt}...master`]);
+  const files = cmd.stdout.trim();
+  if (files.includes(".changeset")) {
+    await spawn("yarn");
+    await spawn("yarn", ["changeset", "bump"]);
+    await spawn("git", ["add", "."]);
+    await spawn("git", ["config", "--global", "user.name", "mitchellhamilton"]);
+    await spawn("git", [
+      "config",
+      "--global",
+      "user.email",
+      "mitchell@hamil.town"
+    ]);
+    await spawn("git", ["commit", "-m", '"Bump Packages"']);
+    console.log("committed the things");
+  } else {
+    tools.exit.neutral("No new changesets");
+  }
 });
