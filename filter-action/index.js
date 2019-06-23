34a9f781365bb26d20eb2b0e311a1b41881f77a0
mitchellhamilton <mitchell@hamil.town>
2019-06-23 20:24:35 +1000

    Update a thing

--- filter-action/index.js
+++ filter-action/index.js
@@ -3,10 +3,11 @@ const spawn = require("spawndamnit");
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  let { stdout, stderr } = await spawn("git", [
-    "checkout",
-    "changeset-release-branch"
-  ]);
+  let { stdout, stderr } = await spawn(
+    "git",
+    ["checkout", "changeset-release-branch"],
+    { cwd: tools.workspace }
+  );
   console.log(stdout);
   console.log(stderr);
   tools.exit.neutral("No new changesets");
